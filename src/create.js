86a03d982b9bff160ea5bba74c5f6e6a6d7d72ad
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-08 20:08:27 +0100

    feature: Class 13 - of / range

--- src/create.js
+++ src/create.js
@@ -1,9 +1,22 @@
-import { displayLog } from './utils';
+import { displayLog } from "./utils";
+// of : Secuencia variable de objetos de distinta tipologia
+import { of, range } from "rxjs";
+// range: Para trabajar con una secuencia de números
 
+// Estos dos servirá para crear observables a partir de secuencia de datos
 export default () => {
-    /** start coding */
+  /** start coding */
+  const source = of(1, 2, 3, 4, 5, 6);
+  const sourceTwo = of(
+    [1, 2, 3],
+    "Hello world",
+    { foo: "Bar" },
+    function sayHello() {
+      return "Hi!";
+    }
+  );
+  const sourceThree = range(3, 10);
+  const subscription = sourceThree.subscribe((evt) => displayLog(evt));
 
-
-
-    /** end coding */
-}
+  /** end coding */
+};
