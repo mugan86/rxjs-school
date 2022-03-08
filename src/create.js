0a78ea54e15abce990771702a59b48ed440dc60c
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-08 19:19:16 +0100

    feature: Class 11 - Suscriptions and observers

--- src/create.js
+++ src/create.js
@@ -3,13 +3,26 @@ import { Observable } from 'rxjs';
 
 export default () => {
     /** start coding */
-    const hello = Observable.create(function(observer) {
+    const hello = new Observable(function(observer) {
         observer.next('Hello');
         setTimeout(()=>{
             observer.next('World');
+            observer.complete();
         }, 2000);
+
     });
+
+    // Observador para mosstrar los diferentes estados
+    const observer = {
+        next: evt => displayLog(evt),
+        error: err => console.error("ERROR"),
+        complete: () => displayLog("[DONE]")
+    }
     
-    const subscribe = hello.subscribe(evt => displayLog(evt));
+    const subscribe = hello.subscribe(observer);
+    const subscribe2 = hello.subscribe(observer);
+    // Esto hará que después de emitir "hello" se dessuscriba al ser
+    // "world" algo que se ejecutará de manera asíncrona y no síncrona
+    subscribe.unsubscribe();
     /** end coding */
 }
