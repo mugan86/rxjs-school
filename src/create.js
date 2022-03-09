62fe3c1f41597d5079745b271b48406524e40f27
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-09 08:55:57 +0100

    feature: Class 14 timer / interval finish

--- src/create.js
+++ src/create.js
@@ -1,9 +1,21 @@
 import { displayLog } from './utils';
-
+import { interval, time, timer } from "rxjs";
 export default () => {
     /** start coding */
+    const source = interval(500); // 0.5 sg el intervalo
+    const subscription = source.subscribe((value) => displayLog(value));
+    // a diferencia de "range" y "of" ejecuta una secuencia
+    // que no termina nunca mientras no cancelemos el flujo
+    // Para ello deberíamos de gestionar esa suscripción desde
+    // fuera
 
+    // Podemos hacerlo con un "setTimeOut" y dentro usar
+    // unsubscribe con "subscription"
 
+    /*setTimeout(() => {
+        subscription.unsubscribe();
+    }, 3000)*/
 
+    timer(3000).subscribe(() => subscription.unsubscribe());
     /** end coding */
 }
