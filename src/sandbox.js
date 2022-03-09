c2672aca0d44b441a148072f5791f8c15865eccd
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-09 19:13:41 +0100

    feature: Class 16 OK

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,16 @@
 import { displayLog } from './utils';
-
+import { fromEvent } from "rxjs";
+import { map, filter } from 'rxjs/operators';
 export default () => {
-    /** start coding */
+
     const grid = document.getElementById('grid');
+    const click$ = fromEvent(grid, 'click').pipe(
+        map((point) => [
+            Math.floor(point.offsetX / 50), Math.floor(point.offsetY / 50)]),
+        filter(val => (val[0] + val[1]) % 2 != 0) 
+        // solo filtra la suma de las posiciones impares
+        // que son las que corresponde a la casillas grises
+    );
 
-
-    /** end coding */
+    const subscription = click$.subscribe((point) => displayLog(point))
 }
