5035bb02c8a595866ac6c6caf568ec2e1d082477
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-09 09:34:06 +0100

    feature: Class 15 fromEvent OK

--- src/create.js
+++ src/create.js
@@ -1,9 +1,14 @@
-import { displayLog } from './utils';
-
+import { displayLog } from "./utils";
+import { fromEvent } from "rxjs";
 export default () => {
-    /** start coding */
+  const actionBtn = document.getElementById("action-btn");
+  const source = fromEvent(actionBtn, "click");
 
+  source.subscribe((evt) => {
+    displayLog(`Click event at pos (${evt.x}, ${evt.y})`);
+  });
 
-
-    /** end coding */
-}
+  fromEvent(document, "mousemove").subscribe((evt) => {
+    console.log(evt);
+  });
+};
