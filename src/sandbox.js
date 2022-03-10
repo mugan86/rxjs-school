dbca9741de3261f612dab69845eb466519860316
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-10 18:36:43 +0100

    feature: Class 17 OK

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,14 @@
-import { displayLog } from './utils';
-import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { displayLog } from "./utils";
+import { fromEvent } from "rxjs";
+import { map, tap } from "rxjs/operators";
 
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ])
-    );
+  const grid = document.getElementById("grid");
+  const click$ = fromEvent(grid, "click").pipe(
+    tap((val) => console.log("Before:", val)), // Muestra la información tal y como viene del origen
+    map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]), // Transforma a lo que nosotros indicamos
+    tap((val) => console.log(`After: ${val}`)) // Muestra la información transformada
+  );
 
-    const subscription = click$.subscribe(data => displayLog(data));
-
-    /** end coding */
-}
+  const subscription = click$.subscribe((data) => displayLog(data));
+};
