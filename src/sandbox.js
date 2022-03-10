0dc372a651af9df81131208b9cb12ec1c033c0ea
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-10 19:37:55 +0100

    feature: Class 19 OK

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, takeWhile } from 'rxjs/operators';
+import { map, takeLast, takeWhile, tap, skip} from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -10,7 +10,13 @@ export default () => {
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
         ]),
-        takeWhile( ([col, row]) => col > 3 )
+        //takeWhile( ([col, row]) => col > 3 ),
+        // takeWhile lo usamos con last y takeLast
+        tap(val => console.log(`Cell: [${val}`)),
+        skip(3)
+        // last() // Obtener el último válido (takeWhile) cuando el flujo se cierra
+        // takeLast(3) 
+        // Obtener los últimos n elementos válidos cuando se cierre el flujo
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
