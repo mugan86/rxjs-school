b4cc8c6f6658c81f6ad3a2d8847dab9e6a29c1d7
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-10 19:03:45 +0100

    feature: Class 18 OK

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,18 @@
-import { displayLog } from './utils';
-import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { displayLog } from "./utils";
+import { fromEvent } from "rxjs";
+import { map, takeWhile} from "rxjs/operators";
 
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ])
-    );
+  
+  const grid = document.getElementById("grid");
+  const click$ = fromEvent(grid, "click").pipe(
+    map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+    // first
+    // first([col, row]) => col > 3)
+    // take(4)
+    takeWhile(([col, row]) => col > 3)
+  );
 
-    const subscription = click$.subscribe(data => displayLog(data));
+  const subscription = click$.subscribe((data) => displayLog(data));
 
-    /** end coding */
-}
+};
