44e94a176e9a8f937556188cab2d2db166f39b6b
Anartz Mugika Ledo <mugan86@gmail.com>
2022-10-10 08:24:41 +0200

    class 22 - distinctUntilChanged

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,21 +1,20 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, tap, distinct } from 'rxjs/operators';
+import { map, tap, distinctUntilChanged } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
         // Obtener el valor de la columna y fila
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
+        map(val => [
+            Math.floor(val.offsetX / 50),
+            Math.floor(val.offsetY / 50)
         ]),
-        map((val) => `${val[0]}-${val[1]}`),
         tap(val => console.log('select column / row', val)),
-        // Mostrará en la UI si el valor de la suma de columna + fila
-        // es diferente a lo que se ha mostrado anteriormente
-        distinct()
+        // Mostrará en la UI el valor si lo que comparamos es DIFERENTE
+        // Solo bloquea clicks consecutivos al mismo punto
+        distinctUntilChanged((cellOne, cellTwo) => cellOne[0] === cellTwo[0] && cellOne[1] === cellTwo[1])
     );
 
     click$.subscribe(data => displayLog(data));
